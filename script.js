ae26d747e906d9f3c239ec60ca63f927ae286247
Jennifer Walason <jenniferwalason@gmail.com>
2020-06-01 08:16:34 -0400

    code refactor and js btn

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// header code
 $(function() {
     $(window).scroll(function() {
       var winTop = $(window).scrollTop();
@@ -7,5 +8,30 @@ $(function() {
         $("body").removeClass("sticky-shrinknav-wrapper");
       }
     });
+
   });
-  
+ 
+
+function createRecipe() {
+
+  // var queryURL = "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3";
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // }).then(function(response) {
+  //   console.log(queryURL)
+  //   console.log(response);
+ 
+  };
+
+
+
+// Event handler for user clicking the bacon button
+$(".bacon-btn").on("click", function() {
+
+  // Storing the input value
+  var inputNumber = $("#recipe-input").val();
+  console.log(inputNumber)
+  // runs function to display recipes
+  createRecipe(); 
+});
